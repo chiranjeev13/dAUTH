23ff9d60595f3fbdfc0e2f48c69f50b8704383f3
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2023-05-08 00:08:00 +0530

    Update 01-deploy-NFT-MINT.js

--- deploy/01-deploy-NFT-MINT.js
+++ deploy/01-deploy-NFT-MINT.js
@@ -5,7 +5,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deployer } = await getNamedAccounts();
 
   console.log("----------");
-  const args = [];
+  const args = []; // pass the address of the dApp (generate a random hash addr and put it inside the .env) after deployment you can also remove it from your project if you don't require it
   const NFT_MINT = await deploy("NFT_MINT", {
     from: deployer,
     args: args,
